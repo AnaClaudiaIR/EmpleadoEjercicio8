5d8f92b1dcac9f2b154f17fe4ba8dc39cdedede7
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-03-25 12:33:28 +0100

    subir2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,39 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+package org.example;
+
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         try (Connection connection = DriverManager.getConnection(
                 DBConfig.getUrl(),
                 DBConfig.getUser(),
                 DBConfig.getPassword()
-        )) {
-            System.out.println("Connected to database.");
+        );  Statement statement = connection.createStatement()) {
+
+            System.out.println("Introduce la letra: ");
+            String letra = sc.nextLine();
+
+            //String para la consulta
+            String sql = "SELECT * from empleado " +
+                    "WHERE NOMBRE LIKE ?";
+
+            //Prepared statement para introducir la letra
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "%"+letra+"%");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //Recorre todos los nombres e imprime los que cumplan la condición
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nombre = resultSet.getString("nombre");
+                double salario = resultSet.getDouble("salario");
+                System.out.println("\nID: " + id);
+                System.out.println("Nombre: " + nombre);
+                System.out.println("Salario: " + salario);
+            }
         } catch (SQLException e){
             System.out.println("ERROR --> "+e.getMessage());
         }
